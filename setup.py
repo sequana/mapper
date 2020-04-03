ce7cff51c46c840788341597d1ba7b565729296c
Thomas Cokelaer <cokelaer@gmail.com>
2020-04-03 16:13:31 +0200

    Fix rule to cope with null coverage

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_namespace_packages
 
 _MAJOR               = 0
 _MINOR               = 8
-_MICRO               = 8
+_MICRO               = 10
 version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
 release              = '%d.%d' % (_MAJOR, _MINOR)
 
