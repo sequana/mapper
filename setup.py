4b825db2fdac0dd20a0cf7e4b18623a68c4ffd84
Thomas Cokelaer <cokelaer@gmail.com>
2022-02-24 18:45:40 +0100

    add test for the capture

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from setuptools.command.install import install
 import subprocess
 
 _MAJOR               = 0
-_MINOR               = 10
-_MICRO               = 2
+_MINOR               = 11
+_MICRO               = 0
 version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
 release              = '%d.%d' % (_MAJOR, _MINOR)
 
