97695cc0282bfe557f0d8867a7c61e605558a9a4
Thomas Cokelaer <cokelaer@gmail.com>
2023-11-25 23:25:27 +0100

    fix apptainer, simplify onsucess, update schema

--- sequana_pipelines/mapper/main.py
+++ sequana_pipelines/mapper/main.py
@@ -2,7 +2,7 @@
 #  This file is part of Sequana software
 #
 #  Copyright (c) 2016-2021 - Sequana Development Team
-##
+#
 #  Distributed under the terms of the 3-clause BSD license.
 #  The full license is in the LICENSE file, distributed with this software.
 #
