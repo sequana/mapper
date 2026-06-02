44a68528bb49ff9554e01e49cbac073ba0f6a239
Thomas Cokelaer <cokelaer@gmail.com>
2026-06-02 10:11:11 +0200

    update doc and container

--- sequana_pipelines/mapper/__init__.py
+++ sequana_pipelines/mapper/__init__.py
@@ -1,10 +1,12 @@
 import importlib.metadata as metadata
 
+
 def get_package_version(package_name):
     try:
         version = metadata.version(package_name)
         return version
     except metadata.PackageNotFoundError:
         return f"{package_name} not found"
+
 
 version = get_package_version("sequana-mapper")
